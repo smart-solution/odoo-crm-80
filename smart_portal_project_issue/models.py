b6c076d148160a47ae1f702439dc98baff99a558
John Doe <fabian.semal@smartsolution.be>
2015-05-04 10:55:03 +0200

    Alain changes

--- smart_portal_project_issue/models.py
+++ smart_portal_project_issue/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from mock import self
 
 class res_users(models.Model):
      _inherit = 'res.users'
@@ -21,6 +22,7 @@ class project_issue(models.Model):
     in_warranty = fields.Boolean('Under Warranty')
     estimated_time = fields.Float('Estimated Duration')
     project_ids = fields.One2many('project.project', 'partner_id', string='Projects')
+    stage_name = fields.Char(related='stage_id.name')
 
     def create(self, cr, uid, vals, context=None):
         """Fills the issue project with the user default project for portal issues"""
@@ -41,3 +43,46 @@ class project_issue(models.Model):
         return super(project_issue, self).search(cr, uid, args=args, offset=offset, limit=limit, order=order,
             context=context, count=count)
 
+    @api.one
+    def action_to_plan(self, vals):
+        stage_id = self.env['project.task.type'].search([('name', '=', 'To plan (Smart)')])
+        issue = self.env['project.issue'].sudo().search([('id', '=', self.id)])
+        return issue.write({'stage_id': stage_id[0].id})
+		
+    @api.one
+    def action_to_qualify(self, vals):
+        stage_id = self.env['project.task.type'].search([('name', '=', 'To qualify (Smart)')])
+        issue = self.env['project.issue'].sudo().search([('id', '=', self.id)])
+        return issue.write({'stage_id': stage_id[0].id})
+        
+    @api.one
+    def action_to_release(self, vals):
+        stage_id = self.env['project.task.type'].search([('name', '=', 'To release (Smart)')])
+        issue = self.env['project.issue'].sudo().search([('id', '=', self.id)])
+        return issue.write({'stage_id': stage_id[0].id})
+		
+    @api.one
+    def action_in_progress(self, vals):
+        stage_id = self.env['project.task.type'].search([('name', '=', 'In progress (Smart)')])
+        issue = self.env['project.issue'].sudo().search([('id', '=', self.id)])
+        return issue.write({'stage_id': stage_id[0].id})
+        
+    @api.one
+    def action_to_close(self, vals):
+        stage_id = self.env['project.task.type'].search([('name', '=', 'Done')])
+        issue = self.env['project.issue'].sudo().search([('id', '=', self.id)])
+        return issue.write({'stage_id': stage_id[0].id})
+		
+    @api.one
+    def action_cancel(self, vals):
+        stage_id = self.env['project.task.type'].search([('name', '=', 'Cancelled')])
+        issue = self.env['project.issue'].sudo().search([('id', '=', self.id)])
+        return issue.write({'stage_id': stage_id[0].id})
+		
+    @api.one
+    def action_hold(self, vals):
+        stage_id = self.env['project.task.type'].search([('name', '=', 'On hold')])
+        issue = self.env['project.issue'].sudo().search([('id', '=', self.id)])
+        return issue.write({'stage_id': stage_id[0].id})
+		
+		
